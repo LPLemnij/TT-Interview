ccbb8d4fb64cf6a289e21432b1a05e740902556f
Luca <luca@pop-os.localdomain>
2022-05-19 15:56:03 -0400

    added second problem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import CountdownComponentParent from './Countdown/Countdown.js'
+import FetchingComponent from "./Fetching/Fetching.js"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div style={{marginBottom: "20px"}} >Problem 1</div>
       <CountdownComponentParent />
+      <div style={{marginTop: "20px"}}>Problem 2</div>
+      <FetchingComponent />
     </div>
   );
 }
---
+++ src/Fetching/Fetching.js
@@ -0,0 +1,10 @@
+// This excercise is meant for you to showcase what you can do from a programming perspective and how well you can interact with apis, etc. 
+// What you have to do is make a button that fetches data from 'https://jsonplaceholder.typicode.com/posts' and displays the title and 
+// first word of the body in a scrollable table. No need to worry about making it look good too much, this is all about React and your 
+// approach to React programming.
+
+function Fetching() {
+    return <div>Table Goes Here</div>
+}
+
+export default Fetching
