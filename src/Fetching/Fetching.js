8e37b930a0528af10db8029b37393d0960d265ed
Luca <luca@pop-os.localdomain>
2022-05-19 15:59:58 -0400

    added clicked

--- src/Fetching/Fetching.js
+++ src/Fetching/Fetching.js
@@ -1,5 +1,5 @@
 // This excercise is meant for you to showcase what you can do from a programming perspective and how well you can interact with apis, etc. 
-// What you have to do is make a button that fetches data from 'https://jsonplaceholder.typicode.com/posts' and displays the title and 
+// What you have to do is make a button that fetches data from 'https://jsonplaceholder.typicode.com/posts' when clicked and displays the title and 
 // first word of the body in a scrollable table. No need to worry about making it look good too much, this is all about React and your 
 // approach to React programming.
 
